38180ee18f24040255bb7be6b874f72c27bf9ac6
Arisforprogramming <codegarden23@gmail.com>
2024-11-18 23:19:03 -0600

    Task 3 completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 --Part 1
-
+    --Columns :
+      --INTEGER id
+      --VARCHAR employer
+      --VARCHAR name
+      --VARCHAR skill
 --Part 2
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required.")
+    @Size(min = 1, max = 100)
+    public String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    //    public void setLocation(String saintLouis) {
+    // }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
